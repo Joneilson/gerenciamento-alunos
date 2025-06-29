ada6ca728b21940a9c9be5386c5f56a7a6f35dfb
Joneilson Junior <joneilsonjunior@gmail.com>
2025-06-28 23:40:57 -0300

    test: criar testes unitários para ServiceAluno

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,10 +1,46 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.enums.*;
+import br.com.gerenciamento.model.Aluno;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoRepositoryTest {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Test
+    public void salvarAluno() {
+        Aluno aluno = new Aluno("Ana Silva", "112233", Curso.DIREITO, Turno.MATUTINO, Status.ATIVO);
+        Aluno salvo = alunoRepository.save(aluno);
+        Assert.assertNotNull(salvo.getId());
+    }
+
+    @Test
+    public void buscarPorMatricula() {
+        alunoRepository.save(new Aluno("Bruno Costa", "998877", Curso.CONTABILIDADE, Turno.MATUTINO, Status.ATIVO));
+        Aluno aluno = alunoRepository.findByMatricula("998877");
+        Assert.assertNotNull(aluno);
+    }
+
+    @Test
+    public void removerAluno() {
+        Aluno aluno = alunoRepository.save(new Aluno("Carlos Souza", "123789", Curso.DIREITO, Turno.NOTURNO, Status.ATIVO));
+        alunoRepository.delete(aluno);
+        Aluno encontrado = alunoRepository.findByMatricula("123789");
+        Assert.assertNull(encontrado);
+    }
+
+    @Test
+    public void listarTodosAlunos() {
+        alunoRepository.save(new Aluno("Daniele Lima", "555666", Curso.INFORMATICA, Turno.NOTURNO, Status.ATIVO));
+        Assert.assertFalse(alunoRepository.findAll().isEmpty());
+    }
 }
--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -5,7 +5,8 @@ import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
 import jakarta.validation.ConstraintViolationException;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,29 +20,42 @@ public class AlunoServiceTest {
     private ServiceAluno serviceAluno;
 
     @Test
-    public void getById() {
-        Aluno aluno = new Aluno();
-        aluno.setId(1L);
-        aluno.setNome("Vinicius");
-        aluno.setTurno(Turno.NOTURNO);
-        aluno.setCurso(Curso.ADMINISTRACAO);
-        aluno.setStatus(Status.ATIVO);
-        aluno.setMatricula("123456");
-        this.serviceAluno.save(aluno);
+    public void getByIdExistente() {
+        Aluno aluno = new Aluno("Vinicius Almeida", "123456", Curso.ADMINISTRACAO, Turno.NOTURNO, Status.ATIVO);
+        Aluno salvo = this.serviceAluno.save(aluno);
 
-        Aluno alunoRetorno = this.serviceAluno.getById(1L);
-        Assert.assertTrue(alunoRetorno.getNome().equals("Vinicius"));
+        Aluno alunoRetorno = this.serviceAluno.getById(salvo.getId());
+        Assert.assertNotNull(alunoRetorno);
+        Assert.assertEquals("Vinicius Almeida", alunoRetorno.getNome());
     }
 
     @Test
     public void salvarSemNome() {
         Aluno aluno = new Aluno();
-        aluno.setId(1L);
+        aluno.setMatricula("123456");
         aluno.setTurno(Turno.NOTURNO);
         aluno.setCurso(Curso.ADMINISTRACAO);
         aluno.setStatus(Status.ATIVO);
-        aluno.setMatricula("123456");
+
         Assert.assertThrows(ConstraintViolationException.class, () -> {
-                this.serviceAluno.save(aluno);});
+            serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    public void atualizarNomeAluno() {
+        Aluno aluno = new Aluno("Maria", "2025001", Curso.DIREITO, Turno.MATUTINO, Status.ATIVO);
+        Aluno salvo = serviceAluno.save(aluno);
+
+        salvo.setNome("Maria Clara");
+        Aluno atualizado = serviceAluno.save(salvo);
+
+        Assert.assertEquals("Maria Clara", atualizado.getNome());
+    }
+
+    @Test
+    public void buscarAlunoInexistente() {
+        Aluno aluno = serviceAluno.getById(999L);
+        Assert.assertNull(aluno);
     }
 }
